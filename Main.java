3dcb1bd0d721f7bc79f1db97e584dfa6940eddd0
SergRKrus <50547266+SergRKrus@users.noreply.github.com>
2021-04-21 10:33:14 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //ReadText.readUsingScanner("input.txt");
+        //ReadText.writeCollections();
+        GUI.gui();
+    }
+}
